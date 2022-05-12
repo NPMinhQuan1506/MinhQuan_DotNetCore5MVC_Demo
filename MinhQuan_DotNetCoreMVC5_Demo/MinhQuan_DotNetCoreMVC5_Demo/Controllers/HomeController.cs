735937ae8bd3386c08bd72eb286407088ce71dd8
NP Minh Quan <npminhquan1506@gmail.com>
2022-05-13 01:00:47 +0700

    Apply Services and Dependency Injection SMTP gmail

--- MinhQuan_DotNetCoreMVC5_Demo/MinhQuan_DotNetCoreMVC5_Demo/Controllers/HomeController.cs
+++ MinhQuan_DotNetCoreMVC5_Demo/MinhQuan_DotNetCoreMVC5_Demo/Controllers/HomeController.cs
@@ -15,9 +15,10 @@ namespace MinhQuan_DotNetCoreMVC5_Demo.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        readonly Services.SmtpHandling _smtpHandlingServices;
         public HomeController(ILogger<HomeController> logger)
         {
+            _smtpHandlingServices = new Services.SmtpHandling();
             _logger = logger;
         }
 
@@ -44,29 +45,43 @@ namespace MinhQuan_DotNetCoreMVC5_Demo.Controllers
                 return View(formData);
             }
             //TestSMTPQuan
-            using (var client = new SmtpClient())
-            {
-                client.Connect("smtp.gmail.com");
-                client.Authenticate("TestSMTPQuan@gmail.com", "123@@Abc");
+            /*
+             Step 1: Install mailkit
+             Step 2: using MailKit.Net.Smtp, using MimeKit
+             Step 3: Set enable Less secure app access in manage your account google/Secure
+             Step 4: Click setting icon in gmail, then choose 'see all setting', go to ...POP/IMAP tab -> enable IMAP access
+             Step 5: using SmtpClient
+                  5.1: set bodyBuilder
+                  5.2: set message
+                  5.3: Add message from gmail
+                  5.4: Add message to gmail
+                  5.5: client send message then disconect client
+            */
 
-                var bodyBuilder = new BodyBuilder
-                {
-                    HtmlBody = $"<p>{formData.Id}</p> <p>{formData.Name}</p> <p>{formData.Phone}</p> <p>{formData.Email}</p> <p>{formData.Note}</p>",
-                    TextBody = "{formData.Id} \r\n {formData.Name} \r\n {formData.Phone} \r\n {formData.Email} \r\n {formData.Note}"
-                };
+            //using (var client = new SmtpClient())
+            //{
+            //    client.Connect("smtp.gmail.com");
+            //    client.Authenticate("TestSMTPQuan@gmail.com", "123@@Abc");
 
-                var message = new MimeMessage
-                {
-                    Body = bodyBuilder.ToMessageBody() 
-                };
+            //    var bodyBuilder = new BodyBuilder
+            //    {
+            //        HtmlBody = $"<p>{formData.Id}</p> <p>{formData.Name}</p> <p>{formData.Phone}</p> <p>{formData.Email}</p> <p>{formData.Note}</p>",
+            //        TextBody = "{formData.Id} \r\n {formData.Name} \r\n {formData.Phone} \r\n {formData.Email} \r\n {formData.Note}"
+            //    };
 
-                message.From.Add(new MailboxAddress("Noreply my site", "TestSMTPQuan@gmail.com"));
-                message.To.Add( new MailboxAddress("Testing01", formData.Email));
-                message.Subject = "New contact submit";
-                
-                client.Send(message);
-                client.Disconnect(true);
-            }
+            //    var message = new MimeMessage
+            //    {
+            //        Body = bodyBuilder.ToMessageBody()
+            //    };
+
+            //    message.From.Add(new MailboxAddress("Noreply my site", "TestSMTPQuan@gmail.com"));
+            //    message.To.Add(new MailboxAddress("Testing01", formData.Email));
+            //    message.Subject = "New contact submit";
+
+            //    client.Send(message);
+            //    client.Disconnect(true);
+            //}
+            _smtpHandlingServices.sendEmail(formData.Id, formData.Name, formData.Phone, formData.Email, formData.Note);
             TempData["Message"] = "Thank you! We'll contact you shortly!";
             return RedirectToAction("Contact");
             //return Json(formData);
---
+++ MinhQuan_DotNetCoreMVC5_Demo/MinhQuan_DotNetCoreMVC5_Demo/Services/SmtpHandling.cs
@@ -0,0 +1,48 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+namespace MinhQuan_DotNetCoreMVC5_Demo.Services
+{
+    public class SmtpHandling
+    {
+        public void sendEmail( int id, string name, string phone, string email, string note)
+        {
+            //TestSMTPQuan
+            /*
+             Step 1: Install mailkit
+             Step 2: using MailKit.Net.Smtp, using MimeKit
+             Step 3: Set enable Less secure app access in manage your account google/Secure
+             Step 4: Click setting icon in gmail, then choose 'see all setting', go to ...POP/IMAP tab -> enable IMAP access
+             Step 5: using SmtpClient
+                  5.1: set bodyBuilder
+                  5.2: set message
+                  5.3: Add message from gmail
+                  5.4: Add message to gmail
+                  5.5: client send message then disconect client
+            */
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com");
+                client.Authenticate("TestSMTPQuan@gmail.com", "123@@Abc");
+
+                var bodyBuilder = new BodyBuilder
+                {
+                    HtmlBody = $"<p>{id}</p> <p>{name}</p> <p>{phone}</p> <p>{email}</p> <p>{note}</p>",
+                    TextBody = "{id} \r\n {name} \r\n {phone} \r\n {email} \r\n {note}"
+                };
+
+                var message = new MimeMessage
+                {
+                    Body = bodyBuilder.ToMessageBody()
+                };
+
+                message.From.Add(new MailboxAddress("Noreply my site", "TestSMTPQuan@gmail.com"));
+                message.To.Add(new MailboxAddress("Testing01", email));
+                message.Subject = "New contact submit";
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
